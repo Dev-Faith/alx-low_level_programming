f39d42f2dd93988dad6bd9a6a52fcbd3e8e23ec7
Faith Adebayo <adebayofaith0@gmail.com>
2022-06-14 12:17:17 -0700

    viic

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
  * _strlen - swaps values of two integers
  * @s: param
  */
 int _strlen(char *s)
 {
+int len;
 len = strlen(s);
 return (len);
 }
