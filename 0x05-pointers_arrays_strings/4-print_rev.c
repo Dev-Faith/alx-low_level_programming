8d67fcfba556c2ef407964e5ce595e3b2938b623
Faith Adebayo <adebayofaith0@gmail.com>
2022-06-14 14:20:42 -0700

    comii

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
 #include "main.h"
-#include <string.h>
 /**
- * print_rev - prints rev
- * @s: param
+ * print_rev - reverse something
+ * @s: params
  */
 void print_rev(char *s)
 {
-puts(strrev(s));
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
 }
