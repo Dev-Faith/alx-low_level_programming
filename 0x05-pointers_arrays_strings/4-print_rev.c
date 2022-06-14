2a215ab9f2a6f49ceed535ded2116a36986cdc7c
Faith Adebayo <adebayofaith0@gmail.com>
2022-06-14 14:55:35 -0700

    cvcvc

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,16 +5,16 @@
  */
 void print_rev(char *s)
 {
-	int i = 0;
-	int j;
+int i = 0;
+int j;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	for (j = i - 1; j >= 0; j--)
-	{
-		_putchar(s[j]);
-	}
-	_putchar('\n');
+while (s[i] != '\0')
+{
+i++;
+}
+for (j = i - 1; j >= 0; j--)
+{
+_putchar(s[j]);
+}
+_putchar('\n');
 }
