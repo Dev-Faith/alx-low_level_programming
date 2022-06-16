d8896440ca52500170f367b1b41741681ffe50e0
Faith Adebayo <adebayofaith0@gmail.com>
2022-06-16 04:43:12 -0700

    hshdf

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,27 @@
-#include <stdio.h>
 #include "main.h"
 /**
-* _strcat - concatenates string values
-* @dest: A pointer to a character to be concatenated
-* @src: A pointer to a character to be concatenated
-*/
+ * _strcat - concatenates two strings
+ * @dest: A pointer to a character that will be changed
+ * @src: A pointer to a character that will also be changed
+ * Return: dest
+ */
 char *_strcat(char *dest, char *src)
 {
-int i;
-int j;
-i=0;
-while (dest[i]!='\0')
-{
-i++;
-}
-j=0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';
+	int i, j;
 
-return (dest);
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
