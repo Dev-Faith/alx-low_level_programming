ce685eaa3f0b757b89baa8f01a254c98d067c1b6
Faith Adebayo <adebayofaith0@gmail.com>
2022-06-16 04:40:36 -0700

    sdb

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,5 +7,21 @@
 */
 char *_strcat(char *dest, char *src)
 {
-strcat(dest, src);
+int i;
+int j;
+i=0;
+while (dest[i]!='\0')
+{
+i++;
+}
+j=0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
 }
