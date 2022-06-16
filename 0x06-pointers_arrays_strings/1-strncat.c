696e7c28ee683b8a8bf15f9cdc0ec3310f4398a9
Faith Adebayo <adebayofaith0@gmail.com>
2022-06-16 06:42:18 -0700

    fjdjd

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,7 +7,15 @@
 * @src: a pointer
 * @n: a param
 */
+
 char *_strncat(char *dest, char *src, int n)
 {
-    strncat(dest, src, n);
+   int len1, len2, i;
+   len1 = strlen(dest);
+   len2 = strlen(src);
+   for (i=0; i<=n; i++)
+   {
+       dest[len1+i]=src[i];
+       printf("Your name: %s\n", dest);
+   }
 }
